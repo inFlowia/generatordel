8f4a1250ab8cb52824663fbcd70f2114ad332735
inFlowia <inFlowia@netc.it>
2022-11-05 13:59:52 +0300

    Рефакторинг IdeaPageTest
    Тесты проходят.

--- tests/Controller/IdeaPageTest.php
+++ tests/Controller/IdeaPageTest.php
@@ -6,13 +6,9 @@ namespace App\Tests\Controller;
 
 use App\Fixtures\IdeaPageTest\IdeaFixture;
 use App\Fixtures\IdeaPageTest\UserFixture;
-use Symfony\Bundle\FrameworkBundle\KernelBrowser;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /** Проверка ЭП: Страница с данными идеи */
-class IdeaPageTest extends WebTestCase
+class IdeaPageTest extends AbstractGetTest
 {
     private const REQUESTED_IDEA_ID = IdeaFixture::REQUESTED_IDEA_ID;
     private const EXPECTED = [
@@ -20,23 +16,11 @@ class IdeaPageTest extends WebTestCase
         'author'  => UserFixture::FIRST_USER_LOGIN,
     ];
 
-    private ?KernelBrowser $client;
-    private ContainerInterface $container;
-    private UrlGeneratorInterface $router;
-
     public function testAction(): void
     {
-        /* todo: вынести инициализацию вспомогательных свойств в абстрактный
-        класс */
-        $this->client = static::createClient();
-        static::bootKernel(['environment' => 'test']);
-        $this->container = static::$kernel->getContainer();
-        $this->router = $this->container->get('router');
-        //
-
-        $this->client->request(
+        self::$client->request(
             'GET',
-            $this->router->generate(
+            self::$router->generate(
                 'idea_page',
                 ['id' => self::REQUESTED_IDEA_ID]
             )
@@ -46,7 +30,7 @@ class IdeaPageTest extends WebTestCase
         self::assertEquals(
             self::EXPECTED,
             \json_decode(
-                $this->client->getResponse()->getContent(),
+                self::$client->getResponse()->getContent(),
                 true,
                 512,
                 JSON_THROW_ON_ERROR
