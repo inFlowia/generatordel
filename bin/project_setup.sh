78a3147e1fefbc8275b06441dd9451622a28f3ba
PixeliaBuilder <PixeliaBuilder@list.ru>
2024-10-27 15:53:53 +0300

    Фикс скрипта настройки проекта для разработки
    Добавлено ожидание готовности mysql

--- bin/project_setup.sh
+++ bin/project_setup.sh
@@ -1,10 +1,67 @@
 #!/bin/bash
 
+DB_READY_CHECKING_TIMEOUT_SEC=60
+DB_READY_CHECKING_INTERVAL_SEC=2
+
+CURRENT_DIR_ABSOLUTE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${CURRENT_DIR_ABSOLUTE_PATH}/../.env"
+source "${CURRENT_DIR_ABSOLUTE_PATH}/../.env.test"
+source "${CURRENT_DIR_ABSOLUTE_PATH}/../.env.test.local"
+
+drawSeparator()
+{
+  echo '============================================================'
+}
+
+waitForDbReady()
+{
+  elapsedTime=0
+
+  while [[ $elapsedTime -lt $DB_READY_CHECKING_TIMEOUT_SEC ]]
+  do
+    echo "Waiting for MySQL..."
+
+    # Эта проверка иногда отвечает успехом раньше готовности к подключению
+    # doctrine:
+    #    docker exec "$DB_CONTAINER_NAME" mysqladmin ping\
+    #     --user="$DB_USER"\
+    #     --password="$MYSQL_ROOT_PASSWORD"\
+    #     --silent
+    # Проверка при помощи запроса надёжнее
+    docker exec "$DB_CONTAINER_NAME" mysql\
+      --user="$DB_USER"\
+      --password="$MYSQL_ROOT_PASSWORD"\
+      --execute="SELECT 'Some response from DB';"\
+      --silent
+
+    exitCode=$?
+
+    if [[ $exitCode -eq 0 ]]
+    then
+      echo "MySQL is ready!"
+
+      return 0
+    fi
+
+    sleep $DB_READY_CHECKING_INTERVAL_SEC
+    elapsedTime=$((elapsedTime + DB_READY_CHECKING_INTERVAL_SEC))
+  done
+
+    echo "Error: MySQL did not become ready within ${DB_READY_CHECKING_TIMEOUT_SEC} seconds."
+    return 1
+}
+
+# ==============================================================================
+
 docker-compose up -d
+drawSeparator
 
 # если в конфиге этого контейнера будет команда, она будет проигнорирована перед
 # этим запуском
 docker-compose run gendel_service_composer composer install
+drawSeparator
 
+waitForDbReady
+drawSeparator
 echo 'Starting project setup success checking script...'
 bin/c
