77bedfe8fca2e5ed47c34e7b1ae6c6fcf2e26aa8
PixeliaBuilder <PixeliaBuilder@list.ru>
2024-11-09 18:49:07 +0300

    rector настроен для конвертирования аннотаций в аттрибуты

--- rector.php
+++ rector.php
@@ -3,14 +3,15 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Symfony\Set\SensiolabsSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
-return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
-    ->withTypeCoverageLevel(0);
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([
+            DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+            SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+            SensiolabsSetList::ANNOTATIONS_TO_ATTRIBUTES,
+        ])
+    ;
+};
