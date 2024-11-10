bc4a03b2baf33340d7a4c20228cb57c2c502ffba
PixeliaBuilder <PixeliaBuilder@list.ru>
2024-11-10 11:34:33 +0300

    Улучшена проверка
    Теперь она завершается если провалена минимальная проверка.

--- bin/check_poject.sh
+++ bin/check_poject.sh
@@ -2,8 +2,16 @@
 CURRENT_DIR_ABSOLUTE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "${CURRENT_DIR_ABSOLUTE_PATH}/lib/cli_decor.sh"
 
-echo "Minimal Check of application functionality starting..."
+
+currentCheckName='Minimal Check of application functionality'
+echo "${currentCheckName} starting..."
 "${CURRENT_DIR_ABSOLUTE_PATH}/mini-check_project.sh"
+currentExitCode=$?
+if [[ $currentExitCode -ne 0 ]]
+then
+  showError "${currentCheckName} finished with error!"
+  exit 1
+fi
 drawSeparator
 
 echo "PHPUnit Tests starting..."
@@ -13,3 +21,4 @@ drawSeparator
 
 echo "Doctrine Schema Validation starting..."
 docker exec -it gendel_container_php bin/console doctrine:schema:validate
+drawSeparator
--- bin/lib/cli_decor.sh
+++ bin/lib/cli_decor.sh
@@ -4,3 +4,14 @@ drawSeparator()
 {
   echo '============================================================'
 }
+
+deleteTabs() {
+	# удаление табуляций
+	echo "${1//"	"/}"
+	# -----------------
+}
+
+showError()
+{
+  echo -en "$(tput setaf 9)$(deleteTabs "$1")$(tput sgr0)\n"
+}
